75c7e9e912ddc926c5a8d9948a86926da0dd1e90
Wenjun Jiang <wenjunjanejiang@gmail.com>
2020-12-05 19:23:37 -0500

    database schema update

--- database/create.sql
+++ database/create.sql
@@ -1,42 +1,29 @@
+DROP DATABASE IF EXISTS sweforumdb; 
+
 CREATE DATABASE sweforumdb;
 \c sweforumdb;
 
+CREATE TABLE subforums (
+  forum_id SERIAL PRIMARY KEY,
+  forum_name VARCHAR(200)
+);
+
 CREATE TABLE users (
-  uid SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE,
   email VARCHAR(255),
-  email_verified BOOLEAN,
-  date_created DATE,
-  last_login DATE
+  email_verified BOOLEAN
 );
 
 CREATE TABLE posts (
-  pid SERIAL PRIMARY KEY,
+  p_id SERIAL PRIMARY KEY,
   title VARCHAR(255),
-  body VARCHAR,
-  search_vector TSVECTOR,
-  user_id INT REFERENCES users(uid),
-  author VARCHAR REFERENCES users(username),
+  forum_id INT REFERENCES subforums(forum_id),
+  content VARCHAR,
+  user_id INT REFERENCES users(user_id),
+  username VARCHAR REFERENCES users(username),
   date_created TIMESTAMP,
-  like_user_id INT[] DEFAULT ARRAY[]::INT[],
-  likes INT DEFAULT 0
-);
-
-CREATE TABLE comments (
-  cid SERIAL PRIMARY KEY,
-  comment VARCHAR(255),
-  author VARCHAR REFERENCES users(username),
-  user_id INT REFERENCES users(uid),
-  post_id INT REFERENCES posts(pid),
-  date_created TIMESTAMP
+  data_lastUpdated TIMESTAMP
 );
 
 
-CREATE TABLE messages (
-  mid SERIAL PRIMARY KEY,
-  message_sender VARCHAR(255) REFERENCES users(username),
-  message_to VARCHAR(255) REFERENCES users(username),
-  message_title VARCHAR(255),
-  message_body VARCHAR,
-  date_created TIMESTAMP
-);
