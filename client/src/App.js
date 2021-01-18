06ec60e7807613b2ee2bef9728b747bb04a6e5a5
Kaivalya Tota <ktota@ucla.edu>
2021-01-18 12:09:02 -0500

    added axios for api requests

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
 class App extends Component {
   state = {
     hello: null
   }
 componentDidMount() {
-  axios.get('/hello')
-    .then(res => this.setState({hello: res.data}))
-    .catch(err => console.log(err))
+  this.asyncFunction()
+}
+
+asyncFunction = async () => {
+  await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(json => console.log(json))
 }
 
   render() {
