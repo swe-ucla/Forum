933c26aad09d7acd9f7914e69b47b0b8aec1385c
Wenjun Jiang <wenjunjanejiang@gmail.com>
2021-01-23 18:44:29 -0800

    updated configuration for testing endpoints

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'mydb',
+  database: 'sweforumdb',
   password: '',
   port: 5432
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../db')
+
+
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
@@ -9,5 +12,50 @@ router.get('/', function(req, res, next) {
 router.get('/hello', function(req, res) {
   res.json('hello world')
 })
+
+
+
+// get posts
+router.get('/api/get/allposts', (req, res, next ) => {
+  pool.query("SELECT * FROM posts ORDER BY date_created DESC", (q_err, q_res) => {
+      res.json(q_res.rows)
+  })
+})
+
+// save post
+router.post('/api/posts/poststodb', (req, res, next) => {
+  const values = [req.body.title, req.body.body, req.body.uid, req.body.username]
+  pool.query('INSERT INTO posts(title, body, user_id, username, date_created) VALUES($1, $2, $3, $4, NOW())', values, (q_err, q_res) => {
+      if(q_err) return next(q_err);
+      res.json(q_res.rows)
+  })
+})
+
+// update posts
+router.put('/api/put/post', (req, res, next) => {
+  const value = [req.body.title, req.body.body, req.body.uid, req.body.pid, req.body.username]
+  pool.query('UPDATE posts SET title=$1, body=$2, user_id=$3, username=$5, date_created=NOW() WHERE pid = $4', values, (q_err, q_res) => {
+      res.json(q_res.rows)
+  })
+})
+
+// delete post comments
+router.delete('/api/delete/postcomments'), (req, res, next) => {
+  const post_id = req.body.post_id
+  pool.query('DELETE FROM comments WHERE post_id = $1', [post_id], (q_err, q_res) => {
+      res.json(q_res.rows)
+      console.log(q_err)
+  })
+}
+
+// delete post
+router.delete('/api/delete/post'), (req, res, next) => {
+  const post_id = req.body.post_id
+  pool.query('DELETE FROM posts WHERE pid = $1', [post_id], (q_err, q_res) => {
+      res.json(q_res.rows)
+      console.log(q_err)
+  })
+}
+
 
 module.exports = router;
